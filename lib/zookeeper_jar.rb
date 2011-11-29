5c2f3e0fdfc7dee3613662d5ae6606f059d9a101
Jonathan D. Simms <slyphon@gmail.com>
2011-11-29 01:03:28 +0000

    add new jar file, bump version

--- lib/zookeeper_jar.rb
+++ lib/zookeeper_jar.rb
@@ -1,5 +1,6 @@
 module ZookeeperJar
 end
 
-require File.expand_path('zookeeper-3.3.3.jar', File.dirname(__FILE__))
 require 'zookeeper_jar/version'
+require File.expand_path("../zookeeper-#{ZookeeperJar::VERSION}.jar", __FILE__)
+
