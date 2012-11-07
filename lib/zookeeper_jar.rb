167b4794f2fd93835f0cbf2aadee42394ed31ca3
Jonathan D. Simms <slyphon@twitter.com>
2012-11-07 02:41:03 +0000

    just add the rest of the jar dependencies

--- lib/zookeeper_jar.rb
+++ lib/zookeeper_jar.rb
@@ -1,6 +1,19 @@
 module ZookeeperJar
+  SLF4J_VERSION = '1.6.1' unless defined?(SLF4J_VERSION)
+
+  def self.require_relative(*names)
+    names.each do |name|
+      require File.expand_path(File.join('..', name), __FILE__)
+    end
+  end
 end
 
 require 'zookeeper_jar/version'
-require File.expand_path("../zookeeper-#{ZookeeperJar::VERSION}.jar", __FILE__)
+
+ZookeeperJar.require_relative(*%W[
+  log4j-1.2.15.jar
+  slf4j-api-#{ZookeeperJar::SLF4J_VERSION}.jar
+  slf4j-log4j12-#{ZookeeperJar::SLF4J_VERSION}.jar
+  zookeeper-#{ZookeeperJar::VERSION}.jar
+])
 
